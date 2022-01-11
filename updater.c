1194bda641cee40591e454abc0d85e60f2dc1e4e
munvoseli <muslinmelody@gmail.com>
2022-01-11 15:06:59 -0500

    time logging potato when new frame occurs

--- updater.c
+++ updater.c
@@ -7,18 +7,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 void stateCallback(void* datap, size_t size, size_t nmemb, void* userp) {
 	char* cdatap = (char*) datap;
 	char* cuserp = (char*) userp;
 	char isDifferent = strncmp(cdatap + 8, cuserp, 64);
+	time_t utcSec = time(NULL);
+	struct tm* utcTim = gmtime(&utcSec); // Tim is the keeper of all clocks
+	printf("%d-%d %d:%d     ", utcTim->tm_mon + 1, utcTim->tm_mday, utcTim->tm_hour, utcTim->tm_sec);
 	if (isDifferent) {
 		printf("Different!\n%s\n", cdatap);
 		for (unsigned char i = 0; i < 64; ++i) {
 			(cuserp)[i] = (cdatap)[i + 8];
 		}
 		FILE* fp = fopen("frames.txt", "a");
-		fprintf(fp, "%s\n", cdatap);
+		fprintf(fp, "%s   %d-%d %d:%d\n", cdatap, utcTim->tm_mon + 1, utcTim->tm_mday, utcTim->tm_hour, utcTim->tm_sec);
 		fclose(fp);
 	} else {
 		printf("Same.   %s\n", cuserp);
