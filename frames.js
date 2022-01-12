592cb92d79709d301190f7551ee4035d845d25f4
munvoseli <muslinmelody@gmail.com>
2022-01-12 08:47:02 -0500

    13th frame

--- frames.js
+++ frames.js
@@ -1,9 +1,13 @@
 // https://xkcd.com/count-wimRikmef/state
 
 let framesFull = `\
+ program not built yet
 ac84f99b9c41eb75e1a595ea74c7bccf48f36c345d8b88ad5dd478d4520bc0b2 10?16:00 , 10 17:10
+ program not built yet
 6a73d565838a58e4d3069ad6cc49cc1a1466a8e602c95fd752126900d2074e28 10?20:00 , 10 20:46
+ program not built yet
 98b1c93977c76fd31529ff07f8f6f83209ba69ccc5a6b245e3a1061d36093370 11?00:00
+ program not built yet
 154e4cdc63e9b5c6874e0f8cc2146a945c6b53089854fe4827ed6a822fbf8f53 11 04:25 , 11 07:49
 582bbf28305fefb09ea44beae1330585aea155adf6f5125fc70ebdfa6af8c70c 11 08:01:31
 5c65f55f9a51a8b5bb7184a7b42e2930aea829e4d5ef2ff6373d1408fee4cce3 11 12:00:08
@@ -13,8 +17,11 @@ ac84f99b9c41eb75e1a595ea74c7bccf48f36c345d8b88ad5dd478d4520bc0b2 10?16:00 , 10 1
 5a0d51a4f85c24edbd647e18e33044ffe5e49a8474c5f9850dadb742252a5582 11 23:13:14
 85cb9234739c590b3c649c80e6d388f8c53f600327abbdbe476dca495705f86c 12 2:54:42
 62081002ad92456a911eae8eae3f3d74f8147eab8f42330d4f81c473aaa648ba 12 6:40:41
+ my internet went down while i was sleeping
+8883103b355c6cbaa0cc5f78867bfc9fee1e2e4295ccec1a0af1bd4bdd27cc6a 12 13:17:16
 `.split("\n");
 framesFull.pop();
+framesFull = framesFull.filter(x => x[0]!=" ");
 
 let frames = framesFull.map(x=>x.substring(0,64));
 let frameInfos = framesFull.map(x=>x.substring(65));
